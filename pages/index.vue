dbb2c2305f3c6ac4a568efd926d56f3780dfe4ca
kotabuddy <124342189+kotabuddy@users.noreply.github.com>
2023-02-03 13:10:54 +0530

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -64,18 +64,4 @@ Contact Us
 If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me at kotabuddy@email.com
       </p>
     </div>
-    <FeedbackForm />
-    <JokeBlock />
-  </main>
-</template>
-
-<style>
-* {
-  font-family: 'Helvetica', sans-serif;
-}
-.intro {
-  min-width: 400px;
-  padding: 10px 40px;
-  width: 60%;
-}
-</style>
+    
